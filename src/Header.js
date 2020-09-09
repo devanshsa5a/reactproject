ab1dd95b4b39e0a75c36d7252c75389bb35f4a2b
devanshsa5a <mail2devansh@gmail.com>
2020-09-09 21:13:23 +0530

    first

---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './header.css'
+import amzonlogo from './56-565024_amazon-logo-png-amazon-png-transparent-png.png'
+// import PageviewIcon from '@material-ui/icons/Pageview'
+import SearchIcon from '@material-ui/icons/Search';
+function Header() {
+    return (
+        <div className="header">
+            <img className="header_logo" src={amzonlogo} />
+
+            <div className="search_bar">
+                <input className="search_input" type="text" />
+                <SearchIcon />
+            </div>
+            <div className="header_nav">
+                <div className="header_option">
+                    <span className="line_one">Hello Devansh</span>
+                    <span className="line_two">Sign In</span>
+                </div>
+                <div className="header_option">
+                    <span className="line_one">Returns </span>
+                    <span className="line_two"> and Orders</span>
+                </div>
+                <div className="header_option">
+                    <span className="line_one">Your </span>
+                    <span className="line_two">Prime</span>
+                </div>
+
+
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Header
